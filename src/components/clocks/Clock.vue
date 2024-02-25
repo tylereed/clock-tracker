25a7f341fea70729af175863a83a2a6b0ca0921d
Tyler Reed <tylereed@gmail.com>
2024-02-25 13:24:26 -0700

    Display partial filled in piece when hovering
    Squashed commit of the following:
    commit 2354828acb006a9a9fc2c854d2592b6052ba3a48
    Author: Tyler Reed <tylereed@gmail.com>
    Date:   Sun Feb 25 13:21:54 2024 -0700
    
        Fixing issue where checkered fill wasn't being drawn after decrementing a clock
    
    commit f36c37f180fc7bb339c77b50e97324742873f84b
    Author: Tyler Reed <tylereed@gmail.com>
    Date:   Sun Feb 25 08:48:52 2024 -0700
    
        Drawing checkered pattern for hover
    
    commit 5d48311a76f488da3c9784352bc30d5bd0ad4cbe
    Author: Tyler Reed <tylereed@gmail.com>
    Date:   Sat Feb 24 14:55:37 2024 -0700
    
        Changing fill to 30%
    
    commit f804fe6cfbd8f777154f3766815c69cc1a38c965
    Author: Tyler Reed <tylereed@gmail.com>
    Date:   Sat Feb 24 14:47:44 2024 -0700
    
        Adding in transparent filled slice when hovering over clock.
    
    commit d187d51c4af8e066597ac90c1c56f822e43f7a24
    Author: Tyler Reed <tylereed@gmail.com>
    Date:   Sat Feb 24 14:46:30 2024 -0700
    
        Forcing name label to show, even if the clock has no name

--- src/components/clocks/Clock.vue
+++ src/components/clocks/Clock.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-hover>
+  <v-hover @update:model-value="updateHover">
     <template v-slot:default="{ isHovering, props }">
       <v-card v-bind="props" class="d-flex justify-center" @click="increment()" @click.right="decrement()"
         @contextmenu.prevent>
         <v-btn @click.stop="emit('deleteClock', clock.id)" v-show="isHovering" class="topright text-center">
           <v-icon icon="mdi-delete-forever" color="red" />
         </v-btn>
-        <v-sheet :width="size + 15" :height="size + 15 + (name ? 50 : 0)">
-          <v-card-title v-if="name">{{ name }}</v-card-title>
+        <v-sheet :width="size + 15" :height="size + 65">
+          <v-card-title>{{ name || "&nbsp;" }}</v-card-title>
           <canvas ref="canvasRef" class="ma-2" :width="size" :height="size" />
         </v-sheet>
       </v-card>
@@ -30,6 +30,7 @@ import { onMounted, onUpdated, ref } from 'vue';
 const tau = Math.PI * 2;
 
 const clock = defineProps<Clock>();
+let isHover = false;
 
 const emit = defineEmits<{
   (e: "updateSlice", id: number,  filledSlices: number): void,
@@ -49,6 +50,11 @@ function decrement() {
   emit("updateSlice", clock.id, clock.filledSlices - 1);
 }
 
+function updateHover(value: boolean) {
+  isHover = value;
+  render();
+}
+
 function render() {
   if (!canvasRef.value) return;
 
@@ -58,24 +64,46 @@ function render() {
   const radius = Math.min(width, height) / 2 - 2;
   const x = width / 2;
   const y = height / 2;
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext("2d")!;
 
-  if (!ctx) return;
+  ctx.clearRect(0, 0, clock.size, clock.size);
+
   const originAngle = -tau / 4;
 
   let previousAngle = originAngle;
 
   for (let i = 0; i < clock.totalSlices; i++) {
+    ctx.save();
+
     const startAngle = previousAngle;
     previousAngle = (i + 1) * tau / clock.totalSlices + originAngle;
 
+    let fillStyle: string | CanvasPattern;
+    if (i < clock.filledSlices) {
+      fillStyle = clock.color;
+    } else if (i == clock.filledSlices && isHover) {
+      const offscreen = new OffscreenCanvas(10, 10);
+      const offCtx = offscreen.getContext("2d")!;
+      offCtx.fillStyle = clock.color;
+      offCtx.fillRect(0, 0, 5, 5);
+      offCtx.fillRect(5, 5, 10, 10);
+
+      fillStyle = ctx.createPattern(offscreen, "repeat")!;
+      ctx.globalAlpha = .4;
+    } else {
+      fillStyle = "white";
+    }
+
     ctx.beginPath();
-    ctx.fillStyle = i < clock.filledSlices ? clock.color : "white";
+    ctx.fillStyle = fillStyle;
     ctx.moveTo(x, y);
     ctx.arc(x, y, radius, startAngle, previousAngle);
     ctx.closePath();
     ctx.fill();
+    ctx.globalAlpha = 1;
     ctx.stroke();
+
+    ctx.restore();
   }
 }
 </script>
