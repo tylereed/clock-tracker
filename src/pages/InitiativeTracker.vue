61d5e1d59f0940de5e22e6a70a9756aa22452a79
Tyler Reed <tylereed@gmail.com>
2024-10-28 15:34:29 -0600

    Adding page to manage monsters

--- src/pages/InitiativeTracker.vue
+++ src/pages/InitiativeTracker.vue
@@ -4,25 +4,7 @@
       @apply-condition="applyCondition" @remove-condition="removeCondition" @add-initiative="addInitiative"
       @delete-initiative="deleteInitiative" @increment-turn="incrementTurn" @decrement-turn="decrementTurn"
       @reset-turn="resetTurn" @insert-init-command="insertInitCommand" />
-    <v-container fluid>
-      <v-row>
-        <v-col cols="9">
-          <v-autocomplete v-model="monsterSearch" :items="monsters" :custom-filter="monsterNameFilter" return-object
-            auto-select-first item-title="name" item-value="slug">
-            <template v-slot:item="{ props, item }">
-              <v-list-item v-bind="props" :title="''">
-                {{ item.title }} <v-chip density="comfortable" size="x-small">{{ item.raw.document__slug }}</v-chip>
-              </v-list-item>
-            </template>
-          </v-autocomplete>
-          <br />
-          <div><v-btn @click="showLicense = !showLicense">Monster Data License Information</v-btn></div>
-        </v-col>
-        <v-col cols="3">
-          <ts-expando-button class="mt-3" :disabled="!monsterSearch" :actions="addMonsterButtons" />
-        </v-col>
-      </v-row>
-    </v-container>
+    <monster-search @add-monster="addMonster" />
     <v-card-actions>
       <v-btn :disabled="!executor.canUndo.value" @click="() => executor.undo()">
         <v-icon icon="mdi-undo" />
@@ -32,36 +14,23 @@
       </v-btn>
     </v-card-actions>
     <v-dialog v-model="addInitiativeDisplay" width="50%" min-width="400px">
-      <add-edit-initiative class="pa-2 ma-6" :monster-stats="monsterStats" @add-init="addInit"
-        @close="addInitiativeDisplay = false" />
-    </v-dialog>
-    <v-dialog v-model="showLicense" width="75%" min-width="400px">
-      <license />
+      <add-edit-initiative class="pa-2 ma-6" @add-init="addInit" @close="addInitiativeDisplay = false" />
     </v-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount, onMounted, reactive, ref, toRefs } from "vue";
+import { onMounted, ref } from "vue";
 
 import AddEditInitiative from "@/components/initiative/AddEditInitiative.vue";
 import InitiativeTable from "@/components/initiative/InitiativeTable.vue";
-import License from "@/components/initiative/License.vue";
-import TsExpandoButton from "@/components/common/TsExpandoButton.vue";
+import MonsterSearch from "@/components/initiative/MonsterSearch.vue";
 
 import Conditions from "@/types/Conditions";
-import Dice from "@/utils/Dice";
 import { Executor, Command } from "@/utils/Executor";
-import Initiative, { Actions, Initiatives } from "@/types/Initiative";
+import Initiative, { Initiatives } from "@/types/Initiative";
 import * as i from "@/components/initiative/initiativeHelpers";
-import { MonsterNameO5e as MonsterName, getMonsterListCached, getMonsterCached, MonsterO5e } from "@/utils/Open5e";
 
-// const emit = defineEmits<{
-//   (e: "setInitiatives", inits: Initiatives): void,
-// }>();
-
-// const props = defineProps<{ initiatives: Initiatives}>();
-// const { initiatives } = toRefs(props);
 const initiatives = ref<Initiatives>([]);
 
 const columns = i.buildInitiativeColumns({ hasInitiative: true, hasHp: true, hasConditions: true });
@@ -81,12 +50,10 @@ function insertInitiative(init: Initiative) {
     setInitiatives([...initiatives.value, newInit]);
   }, (): void => {
     setInitiatives(initiatives.value.filter(x => x.id !== newInit.id));
-    //initiatives.value = initiatives.value.filter(x => x.id !== newInit.id)
   });
 }
 
 function insertInitiatives(inits: Initiatives, clear: boolean = false) {
-  //const currentId = initId;
   const newInits = inits.map(x => ({ ...x, id: initId++ }));
   const previous = [...initiatives.value];
 
@@ -107,7 +74,6 @@ function resort() {
 }
 
 function setInitiatives(inits: Initiatives) {
-  // emit("setInitiatives", inits);
   initiatives.value = inits.sort((a, b) => b.order - a.order || (b.dex ?? 0) - (a.dex ?? 0));
 }
 
@@ -116,7 +82,6 @@ function deleteInitiative(index: number) {
   const incrementTurn = turn.value > index;
 
   executor.runCommand(() => {
-    //initiatives.value.filter((init, i) => index !== i);
     initiatives.value.splice(index, 1);
     if (turn.value > index) {
       turn.value--;
@@ -174,71 +139,12 @@ function resetTurn() {
 }
 
 const addInitiativeDisplay = ref(false);
-const showLicense = ref(false);
-
-const monsterSearch = ref<MonsterName>();
-const monsterStats = ref<MonsterO5e | null>(null);
-
-onBeforeMount(async () => {
-  monsters.value = await getMonsterListCached();
-});
-
-const monsters = ref<MonsterName[]>([]);
-function monsterNameFilter(title: string, queryText: string): boolean {
-  return title.toLocaleLowerCase().includes(queryText.toLocaleLowerCase());
-}
-
-const addMonsterButtons = reactive([
-  { label: "Add Monster", action: addMonster },
-  { label: "Edit Monster", action: editMonster }
-]);
-
 function addInitiative() {
-  monsterStats.value = null;
   addInitiativeDisplay.value = true;
 }
 
-async function editMonster() {
-  if (monsterSearch.value) {
-    monsterStats.value = await getMonsterCached(monsterSearch.value.slug);
-    addInitiativeDisplay.value = true;
-  }
-}
-
-let nameIndex = 0;
-async function addMonster() {
-  if (monsterSearch.value) {
-    const monster = await getMonsterCached(monsterSearch.value.slug);
-    const letter = String.fromCharCode(65 + nameIndex);
-    nameIndex++;
-    if (nameIndex > 25) {
-      nameIndex = 0;
-    }
-
-    const initMonster: Initiative = {
-      name: `${monster.name} ${letter}`,
-      order: 10 + Dice.calculateModifier(monster.dexterity),
-      dex: monster.dexterity,
-      ac: monster.armor_class,
-      maxHp: monster.hit_points,
-      hp: monster.hit_points,
-      conditions: {},
-      actions: [...buildActions(monster.actions)]
-    }
-
-    insertInitiative(initMonster);
-  }
-}
-
-function* buildActions(...args: ({ name: string, desc: string }[] | undefined)[]): Generator<Actions> {
-
-  for (const arg of args) {
-    if (arg) {
-      for (const a of arg) {
-        yield { name: a.name, desc: a.desc };
-      }
-    }
-  }
+function addMonster(monster: Initiative) {
+  insertInitiative(monster);
 }
 
 function insertInitCommand(index: number, propName: keyof Initiative, newValue: any, oldValue: any) {
@@ -281,5 +187,4 @@ defineExpose({
   insertInitiatives
 });
 
-//initiatives.value = loadInits();
 </script>
--- src/pages/ManageMonsters.vue
+++ src/pages/ManageMonsters.vue
@@ -1,7 +1,218 @@
 <template>
-  
+  <v-container fluid>
+    <v-row>
+      <v-col cols="11">
+        <v-combobox label="Monsters" v-model="search" @update:model-value="setSelected" :items="monsterNames"
+          @keyup.enter="createNewMonster" :hide-no-data="false">
+          <template v-slot:no-data>
+            <v-list-item>
+              <v-list-item-title>
+                No results matching "<strong>{{ search }}</strong>". Press
+                <kbd>enter</kbd> to create a new one
+              </v-list-item-title>
+            </v-list-item>
+          </template>
+        </v-combobox>
+      </v-col>
+      <v-col>
+        <div class="pt-2">
+          <v-btn :disabled="monsterNames.length <= 1">
+            <v-icon icon="mdi-delete-forever" color="error" @click="deleteSelectedMonster" />
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <initiative-table :initiatives="initiatives" :columns="columns" @delete-initiative="deleteInitiative"
+    @insert-init-command="insertInitCommand" />
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-btn variant="elevated" color="primary" @click="addMonster">Add</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn variant="elevated" color="primary" @click="sendToInit">Send to Initiative</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+  <monster-search @add-monster="addExistingMonster" />
+  <v-card-actions>
+    <v-btn :disabled="!executor.canUndo.value" @click="() => executor.undo()">
+      <v-icon icon="mdi-undo" />
+    </v-btn>
+    <v-btn :disabled="!executor.canRedo.value" @click="() => executor.redo()">
+      <v-icon icon="mdi-redo" />
+    </v-btn>
+  </v-card-actions>
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted, ref, watch } from "vue";
 
+import InitiativeTable from "@/components/initiative/InitiativeTable.vue";
+import * as i from "@/components/initiative/initiativeHelpers";
+import MonsterSearch from "@/components/initiative/MonsterSearch.vue";
+
+import { Executor } from "@/utils/Executor";
+import Initiative, { Initiatives, InitWithId } from "@/types/Initiative";
+import { first } from "@/utils/helpers";
+
+let npcId = 0;
+const MonsterNamePrefix = "monster-";
+const FullPrefix = i.makeKey(MonsterNamePrefix)
+
+
+const emit = defineEmits<{
+  (e: "sendToInitiative", inits: Initiatives): void,
+}>();
+
+const allInitiatives = ref<Map<string, Initiatives>>();
+const initiatives = ref<Initiatives>([]);
+const monsterNames = computed(() => allInitiatives.value ? [...allInitiatives.value.keys()] : []);
+const search = ref<string>("Default");
+const selectedMonster = ref<string>("Default");
+
+watch(selectedMonster, (value) => {
+  if (value && allInitiatives.value) {
+    initiatives.value = allInitiatives.value?.get(value) ?? [];
+  } else {
+    initiatives.value = [];
+  }
+});
+
+const columns = i.buildInitiativeColumns({ hasDex: true });
+
+const executor = new Executor(() => i.saveInits(initiatives.value, `${MonsterNamePrefix}${selectedMonster.value}`));
+
+onMounted(() => {
+  allInitiatives.value = new Map<string, Initiatives>(loadAllCombats());
+  npcId = [...allInitiatives.value.values()].flatMap(x => x).map(x => x.order).reduce((x, y) => x > y ? x : y);
+
+  const f = first(monsterNames.value);
+  if (f) {
+    setSelected(f);
+  }
+});
+
+function newNpc(): InitWithId {
+  return {
+    id: npcId++,
+    order: 0,
+    name: "",
+    conditions: {},
+  };
+}
+
+function setSelected(selected?: string | null) {
+  if (selected && allInitiatives.value?.has(selected)) {
+    search.value = selected;
+    selectedMonster.value = selected;
+  }
+}
+
+function createNewMonster() {
+  if (!allInitiatives.value?.has(search.value)) {
+    const selectedMonsterName = selectedMonster.value;
+    const newMonsterName = search.value;
+    const newMonster = [newNpc()];
+
+    executor.runCommand(() => {
+      allInitiatives.value!.set(newMonsterName, [...newMonster]);
+      setSelected(newMonsterName);
+    },
+      () => {
+        setSelected(selectedMonsterName);
+        allInitiatives.value!.delete(newMonsterName);
+      });
+  }
+}
+
+function deleteSelectedMonster() {
+  if (selectedMonster.value && allInitiatives.value?.has(selectedMonster.value)) {
+    const toDeleteKey = selectedMonster.value;
+    const oldMonster = [...initiatives.value];
+
+    executor.runCommand(() => {
+      allInitiatives.value?.delete(toDeleteKey);
+      i.deleteInits(MonsterNamePrefix + toDeleteKey);
+      setSelected(first(monsterNames.value));
+    },
+      () => {
+        allInitiatives.value?.set(toDeleteKey, oldMonster);
+        i.saveInits(oldMonster, MonsterNamePrefix + toDeleteKey);
+        setSelected(toDeleteKey);
+      });
+  }
+}
+
+function addMonster(init?: InitWithId) {
+  const toAdd = init ?? newNpc();
+  const selected = selectedMonster.value;
+
+  executor.runCommand(() => {
+    setSelected(selected);
+    initiatives.value.push({ ...toAdd });
+  },
+    () => {
+      setSelected(selected);
+      initiatives.value.pop();
+    });
+}
+
+function addExistingMonster(monster: Initiative) {
+  addMonster({ ...monster, id: npcId++ });
+}
+
+function sendToInit() {
+  emit("sendToInitiative", [...initiatives.value]);
+}
+
+function deleteInitiative(id: number) {
+  const removed = initiatives.value[id];
+  const selected = selectedMonster.value;
+
+  executor.runCommand(() => {
+    setSelected(selected);
+    initiatives.value.splice(id, 1);
+  },
+    () => {
+      setSelected(selected);
+      initiatives.value.splice(id, 0, removed);
+    });
+}
+
+function insertInitCommand(index: number, propName: keyof Initiative, newValue: any, oldValue: any) {
+  const selected = selectedMonster.value;
+  i.insertInitCommand(executor, initiatives.value, index, propName, newValue, oldValue,
+    () => {
+      setSelected(selected);
+    });
+}
+
+function* loadAllCombats(): Generator<readonly [string, Initiatives], void, unknown> {
+  let returned = false;
+
+  for (const monsterName of loadMonsterNameKeys()) {
+    const monster = i.loadInits(MonsterNamePrefix + monsterName);
+    returned = true;
+    yield [monsterName, monster] as const;
+  }
+
+  if (!returned) {
+    yield ["Default", [newNpc()]] as const;
+  }
+}
+
+function* loadMonsterNameKeys() {
+  const count = localStorage.length;
+  const prefixLength = FullPrefix.length;
+
+  for (let i = 0; i < count; i++) {
+    const key = localStorage.key(i);
+    if (key?.startsWith(FullPrefix)) {
+      yield key.substring(prefixLength);
+    }
+  }
+}
 </script>
