f744e8d3d8980e9ddbb9ccd960415acc41959527
Tyler Reed <tylereed@gmail.com>
2025-03-22 20:49:21 -0600

    Fixing issue where party and monster group wasn't loading correctly

--- src/components/initiative/encounterHelpers.ts
+++ src/components/initiative/encounterHelpers.ts
@@ -1,12 +1,13 @@
 import { computed, Ref } from "vue";
 
 import * as i from "./initiativeHelpers";
+import { Initiatives } from "@/types/Initiative";
 
 export const PartyNamePrefix = "party";
 export const MonsterNamePrefix = "monster";
 
 export function usePrefixes(prefix: Ref<string, string>) {
-  const GroupNamePrefix = computed(() => prefix + "-");
+  const GroupNamePrefix = computed(() => prefix.value + "-");
   const FullPrefix = computed(() => i.makeKey(GroupNamePrefix.value));
 
   return { GroupNamePrefix, FullPrefix };
