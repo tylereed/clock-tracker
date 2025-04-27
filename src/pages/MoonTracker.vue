928594198efadfb33f4fa0436672f750b85abfe9
Tyler Reed <tylereed@gmail.com>
2025-04-26 23:52:39 -0600

    Cleaning up code

--- src/pages/MoonTracker.vue
+++ src/pages/MoonTracker.vue
@@ -40,9 +40,10 @@
 import { nextTick, onMounted, onUpdated, Ref, ref, watch } from "vue";
 import { useTheme } from "vuetify";
 
-import { MoonPhase, MoonSize, MoonState, MoonStateProps } from "@/components/moonTracker/moon";
+import { MoonState, MoonStateProps } from "@/components/moonTracker/moon";
 import { useMoonBonus, useMoonStatus } from "@/components/moonTracker/moonHelpers";
 import MoonStatus from "@/components/moonTracker/MoonStatus.vue";
+import { renderTracker } from "@/components/moonTracker/moonDrawing";
 
 const vTheme = useTheme();
 
@@ -54,7 +55,6 @@ const maxMoonSize = 500;
 
 const canvasRef = ref<HTMLCanvasElement>();
 const size = ref(maxMoonSize);
-
 
 async function resizeCanvas() {
   const padding = 150;
@@ -111,114 +111,6 @@ function randomizeDays() {
   nuitari.value.day = Math.floor(nuitariMax * Math.random());
 }
 
-const tau = Math.PI * 2;
-
-function drawMoon(ctx: CanvasRenderingContext2D, startColor: string, endColor: string, width: number, x: number, y: number, radius: number) {
-  const start = width / 2 - radius;
-  const end = width - start;
-
-  const gradient = ctx.createLinearGradient(0, start, 0, end);
-  gradient.addColorStop(0, startColor);
-  gradient.addColorStop(1, endColor);
-
-  ctx.fillStyle = gradient;
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, tau);
-  ctx.fill();
-  ctx.stroke();
-}
-
-function drawSegments(ctx: CanvasRenderingContext2D, maxSlices: number, x: number, y: number,
-  originAngle: number, innerRadius: number, outerRadius: number) {
-
-  ctx.beginPath();
-  for (let i = 0; i < maxSlices; i++) {
-    const angleOffset = (tau * i / maxSlices);
-    const sin = Math.sin(originAngle + angleOffset);
-    const cos = Math.cos(originAngle + angleOffset);
-
-    const x1 = innerRadius * sin;
-    const y1 = innerRadius * cos;
-    const x2 = outerRadius * sin;
-    const y2 = outerRadius * cos;
-
-    ctx.moveTo(x - x1, y - y1);
-    ctx.lineTo(x - x2, y - y2);
-  }
-  ctx.stroke();
-}
-
-function drawMoons(ctx: CanvasRenderingContext2D, width: number, height: number, x: number, y: number,
-  radiusSolinari: number, radiusLunitari: number, radiusNuitari: number) {
-
-  ctx.clearRect(0, 0, width, height);
-
-  const lineGradient = ctx.createLinearGradient(0, 0, 0, height);
-  lineGradient.addColorStop(0, "#FFD700"); // golden yellow
-  lineGradient.addColorStop(1, "#BFA100");
-
-  ctx.strokeStyle = lineGradient;
-  ctx.lineWidth = 2.5;
-
-  drawMoon(ctx, "#F8F8FF", "#BFC1C2", width, x, y, radiusSolinari); // off-white
-  drawMoon(ctx, "#EF0000", "#6F0000", width, x, y, radiusLunitari);
-  drawMoon(ctx, "#707070", "black", width, x, y, radiusNuitari);
-
-  const originAngle = -Math.PI / 4;
-  drawSegments(ctx, solinariMax, x, y, originAngle, radiusLunitari, radiusSolinari);
-  drawSegments(ctx, lunitariMax, x, y, originAngle, radiusNuitari, radiusLunitari);
-  drawSegments(ctx, nuitariMax, x, y, originAngle, 0, radiusNuitari);
-}
-
-function drawMoonPhase(ctx: CanvasRenderingContext2D, x: number, y: number,
-  miniRadius: number, phaseDisplay: MoonSize, max: number, day: number, radius: number) {
-
-  const originAngle = -Math.PI / 4 + tau / max / 2;
-  const dayAngle = tau * day / max;
-  const xOffset = radius * Math.sin(originAngle + dayAngle);
-  const yOffset = radius * Math.cos(originAngle + dayAngle);
-
-  //ctx.fillText(text, x + xOffset, y + yOffset);
-
-  let fill: string;
-  let secondFill: string;
-  if (phaseDisplay.endsWith("Crescent") || phaseDisplay === "Full Moon") {
-    fill = "yellow";
-    secondFill = "purple";
-  } else {
-    fill = "purple";
-    secondFill = "yellow";
-  }
-
-  ctx.fillStyle = fill;
-  ctx.strokeStyle = fill;
-  ctx.beginPath();
-  ctx.arc(x + xOffset, y + yOffset, miniRadius, 0, tau);
-  ctx.fill();
-  //ctx.stroke();
-  if (phaseDisplay === "Full Moon" || phaseDisplay === "New Moon") {
-    return;
-  }
-
-  // 🌑🌒🌓🌔🌕🌖🌗🌘
-  ctx.fillStyle = secondFill;
-  ctx.strokeStyle = secondFill;
-  //if (!phaseDisplay.endsWith("Crescent")) {
-  const counterCW = phaseDisplay === "Waning Gibbous" || phaseDisplay === "Waxing Crescent" || phaseDisplay.startsWith("Last");
-  ctx.beginPath();
-  ctx.arc(x + xOffset, y + yOffset, miniRadius, -Math.PI / 2, Math.PI / 2, counterCW);
-  // ctx.fill();
-  // ctx.stroke();
-
-  if (!phaseDisplay.endsWith("Quarter")) {
-    //ctx.beginPath();
-    ctx.ellipse(x + xOffset, y + yOffset, miniRadius / 2, miniRadius, 0, -Math.PI / 2, Math.PI / 2, !counterCW);
-  }
-    ctx.fill();
-    ctx.stroke();
-  //}
-}
-
 function render() {
   const canvas = canvasRef.value;
   const width = size.value;
@@ -237,30 +129,8 @@ function render() {
 
   if (!ctx) return;
 
-  ctx.save();
-  drawMoons(ctx, width, height, x, y, radiusSolinari, radiusLunitari, radiusNuitari);
-  ctx.restore();
-
-  const textRadiusSolinari = (radiusSolinari + radiusLunitari) / 2;
-  const textRadiusLunitari = (radiusLunitari + radiusNuitari) / 2;
-  const textRadiusNuitari = 3 * radiusNuitari / 4;
-
-  ctx.save();
-
-  // const fontPercent = width / maxMoonSize;
-  // const fontSize = Math.floor(fontPercent * 30);
-
-  // ctx.font = fontSize + "pt Verdana";
-  // ctx.textAlign = "center";
-  // ctx.textBaseline = "middle";
-
-  const maxMiniMoon = 32;
-  const miniMoonSize = maxMiniMoon * width / maxMoonSize / 2;
-
-  drawMoonPhase(ctx, x, y, miniMoonSize, solinari.value.phaseDisplay, solinariMax, solinari.value.day, textRadiusSolinari);
-  drawMoonPhase(ctx, x, y, miniMoonSize, lunitari.value.phaseDisplay, lunitariMax, lunitari.value.day, textRadiusLunitari);
-  drawMoonPhase(ctx, x, y, miniMoonSize, nuitari.value.phaseDisplay, nuitariMax, nuitari.value.day, textRadiusNuitari);
-  ctx.restore();
+  renderTracker(ctx, width, height, x, y, radiusSolinari, radiusLunitari, radiusNuitari,
+    maxMoonSize, solinari, lunitari, nuitari, solinariMax, lunitariMax, nuitariMax);
 }
 
 </script>
