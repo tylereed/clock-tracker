b53f11bf4365e4225c0120d501b13ee11e83ab8b
Tyler Reed <tylereed@gmail.com>
2025-06-08 11:56:49 -0600

    Adding loading indicator to expando button

--- src/components/common/TsExpandoButton.vue
+++ src/components/common/TsExpandoButton.vue
@@ -2,17 +2,17 @@
   <v-card flat>
     <v-menu transition="scroll-y-transition" :target="mainButton">
       <template v-slot:activator="{ isActive, props }">
-        <v-btn class="mr-0 rounded-s rounded-e-0" @click="mainAction.action()" variant="elevated" color="primary"
-          ref="mainButton">
+        <v-btn :loading="loading" class="mr-0 rounded-s rounded-e-0" @click="runButton(mainAction)" variant="elevated"
+          color="primary" ref="mainButton">
           {{ mainAction.label }}
         </v-btn>
-        <v-btn class="ma-0 rounded-e rounded-s-0" style="min-width: 0;" variant="elevated" color="primary"
-          v-bind="props">
+        <v-btn :disabled="loading" class="ma-0 rounded-e rounded-s-0" style="min-width: 0;" variant="elevated"
+          color="primary" v-bind="props">
           <v-icon :icon="isActive ? 'mdi-menu-down' : 'mdi-menu-up'" />
         </v-btn>
       </template>
       <template v-for="(button, i) in dropdownActions" :key="'button' + i">
-        <v-btn :class="getClass(i)" @click="button.action()" variant="elevated" color="primary">
+        <v-btn :disabled="loading" :class="getClass(i)" @click="runButton(button)" variant="elevated" color="primary">
           {{ button.label }}
         </v-btn>
       </template>
@@ -22,6 +22,8 @@
 
 <script setup lang="ts">
 import { ComponentPublicInstance, computed, ref } from "vue";
+
+const loading = ref(false);
 
 const mainButton = ref<ComponentPublicInstance>(null!);
 
@@ -48,4 +50,14 @@ function getClass(index: number) {
     return ["ma-0", "rounded-0"];
   }
 }
+
+async function runButton(button: ButtonAction): Promise<void> {
+  loading.value = true;
+  try {
+    await Promise.resolve(button.action());
+  } finally {
+    loading.value = false;
+  }
+}
+
 </script>
